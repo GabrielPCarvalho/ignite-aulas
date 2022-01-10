c0c79b7e801bab62a67d990c33118074dd04f0e7
GabrielPCarvalho <gabriel57_pereira@hotmail.com>
2022-01-10 12:04:31 -0300

    userEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
 
 const repository = {
   name: 'unform',
@@ -7,6 +11,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [ repositories, setRepositores] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/GabrielPCarvalho/repos')
+      .then(response => response.json())
+      .then(data => setRepositores(data))
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
